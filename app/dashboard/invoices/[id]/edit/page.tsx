8fcc82d9fa9876fe3fd1e5bfb8727589602450a7
Ali Nauman <ali.nauman@fashion.cloud>
2025-01-05 19:32:12 +0100

    feat: add metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,23 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+import { ResolvingMetadata } from "next";
+
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export const generateMetadata = async (
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+) => {
+  const { id } = await params;
+
+  return {
+    title: `Edit Invoice ${id}`,
+  };
+};
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
